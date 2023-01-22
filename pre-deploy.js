b32bf641fec5e2125f8c2046a8041c09560ef1e1
Anurag <hazru.anurag@gmail.com>
2023-01-22 22:33:40 +0530

    test

--- pre-deploy.js
+++ pre-deploy.js
@@ -9,8 +9,8 @@ const vercelFile = "./vercel.json";
 const vercelJsonContent = fs.readFileSync(vercelFile, "utf-8");
 
 const updated = vercelJsonContent.replace(
-  `"maxDuration": 10`,
-  `"maxDuration": 30`
+  `10`,
+  `30`
 );
 
 fs.writeFileSync(vercelFile, updated, "utf-8");
